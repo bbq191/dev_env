fdffb6f424a39866bd14c502587208a597f204b8
Vinci Xu <xsiyuan@gmail.com>
2023-05-31 09:46:35 +0800

    feat: add many

--- nvimconfig/myconfig/lua/plugins/lspzero.lua
+++ nvimconfig/myconfig/lua/plugins/lspzero.lua
@@ -3,7 +3,7 @@ return {
     branch = 'v2.x',
     dependencies = {
         -- LSP Support
-        { 'neovim/nvim-lspconfig' },     -- Required
+        { 'neovim/nvim-lspconfig' }, -- Required
         {
             -- Optional
             'williamboman/mason.nvim',
@@ -11,18 +11,18 @@ return {
                 pcall(vim.cmd, 'MasonUpdate')
             end,
         },
-        { 'williamboman/mason-lspconfig.nvim' },     -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
         -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },             -- Required
-        { 'hrsh7th/cmp-buffer' },           -- Optional
-        { 'hrsh7th/cmp-path' },             -- Optional
-        { 'hrsh7th/cmp-nvim-lsp' },         -- Required
-        { 'saadparwaiz1/cmp_luasnip' },     -- Optional
-        { 'hrsh7th/cmp-nvim-lua' },         -- Optional
+        { 'hrsh7th/nvim-cmp' },         -- Required
+        { 'hrsh7th/cmp-buffer' },       -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
+        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+        { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
         -- Snippets
-        { 'L3MON4D3/LuaSnip' },                 -- Required
-        { 'rafamadriz/friendly-snippets' },     --Optional
+        { 'L3MON4D3/LuaSnip' },             -- Required
+        { 'rafamadriz/friendly-snippets' }, --Optional
     }
 }
--- nvimconfig/myconfig/lua/user/remaps.lua
+++ nvimconfig/myconfig/lua/user/remaps.lua
@@ -9,23 +9,28 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")       -- zz means center
-vim.keymap.set("n", "<C-u>", "<C-u>zz")       -- always scoll to center
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- zz means center
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- always scoll to center
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v")         -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s")         -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=")         -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>")     -- close current split window
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>")     -- go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>")     -- go to previous tab
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>")    -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>")  -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>")      -- go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>")      -- go to previous tab
 
-vim.keymap.set("n", "<C-t>", ":terminal<CR>")     -- go to previous tab
+vim.keymap.set("n", "<leader>bx", ":bd<CR>")        -- close current buffer
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>")     -- go to next buffer
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>") -- go to previous buffer
+vim.keymap.set("n", "<leader>bl", ":ls<CR>")        -- go to previous buffer
+
+vim.keymap.set("n", "<C-t>", ":terminal<CR>")       -- open  terminal window
 
 -- replays with copy
 vim.keymap.set("x", "<leader>p", [["_dP]])
