f7a904af1272da012ccad69e946958a8059b1c4b
Vinci Xu <xsiyuan@gmail.com>
2023-06-09 17:32:56 +0800

    fix notify bug

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -10,6 +10,7 @@ return {
         },
     },
     opts = {
+        background_colour = "#000000",
         timeout = 15000,
         max_height = function()
             return math.floor(vim.o.lines * 0.75)
@@ -18,13 +19,4 @@ return {
             return math.floor(vim.o.columns * 0.75)
         end,
     },
-    init = function()
-        -- when noice is not enabled, install notify on VeryLazy
-        local Util = require("lazyvim.util")
-        if not Util.has("noice.nvim") then
-            Util.on_very_lazy(function()
-                vim.notify = require("notify")
-            end)
-        end
-    end,
 }
