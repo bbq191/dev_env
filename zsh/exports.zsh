98582e6e735fc84e4190e6e9b0e659973fac1c62
Vinci Xu <xsiyuan@gmail.com>
2023-06-26 09:27:29 +0800

    add npm config

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -11,11 +11,17 @@ export BROWSER="safari"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
+# rust config
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 
+# npm config
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+
+# path append
 export PATH="$CARGO_HOME/bin":$PATH
 # export PATH="$HOME/.local/bin":$PATH
 # eval "$(thefuck --alias)"
 # eval "$(fnm env)"
 eval "$(zoxide init zsh)"
+
