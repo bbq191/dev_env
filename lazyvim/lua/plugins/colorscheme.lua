7bca9feeeac5cf6b46b903ac18e1cc2a6b9e10d1
Vinci Xu <xsiyuan@gmail.com>
2023-06-27 14:31:39 +0800

    change to lazyvim

---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,49 @@
+return {
+
+  -- add rose-pine
+  { "rose-pine/neovim",           name = "rose-pine" },
+
+  -- load github nvim theme
+  { "projekt0n/github-nvim-theme" },
+
+  -- load onedarkpro nvim theme
+  { "olimorris/onedarkpro.nvim" },
+
+  -- Configure LazyVim to load color scheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        local colorscheme = os.getenv("NV_THEME") or "dark"
+        local theme = ""
+        if colorscheme ~= nil then
+          if string.match(colorscheme, "[G|g]it[H|h]ub") ~= nil then
+            theme = "github"
+          else
+            if string.match(colorscheme, "[O|o]ne[D|d]ark") ~= nil then
+              theme = "onedark"
+            else
+              if string.match(colorscheme, "[R|r]ose[P|p]ine") ~= nil then
+                theme = "rosepine"
+              end
+            end
+          end
+          if string.match(colorscheme, "[L|l]ight") then
+            colorscheme = "light"
+            vim.o.background = "light"
+          else
+            colorscheme = "dark"
+            vim.o.background = "dark"
+          end
+        end
+        local has_theme, nvim_theme = pcall(require, "plugins.extras.theme." .. theme)
+        if has_theme then
+          nvim_theme.setup(colorscheme)
+        else
+          require("tokyonight").load() -- fallback to tokyo night if not otherwise specified
+        end
+      end
+    },
+  },
+
+}
---
+++ lazyvim/lua/plugins/extras/theme/rosepine.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.setup(colorscheme)
+  local colorscheme = "rose-pine"
+  local theme = require("rose-pine")
+  local opts = {
+    options = {
+      disable_background = true,
+    },
+  }
+  theme.setup(opts)
+  -- vim.cmd("colorscheme rose-pine")
+  vim.cmd("colorscheme " .. colorscheme)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return M
