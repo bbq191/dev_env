112478181690352408394756c0ae954b6e64531e
Vinci Xu <xsiyuan@gmail.com>
2023-07-27 12:49:10 +0800

    add prettierd

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,6 @@
 #!/bin/s
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-export NV_THEME="rosepine"
 export HISTFILE="$ZDOTDIR/.zhistory"
 HISTSIZE=1000000
 SAVEHIST=1000000
@@ -12,6 +11,7 @@ export BROWSER="safari"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
+export PRETTIERD_LOCAL_PRETTIER_ONLY="$XDG_DATA_HOME/prettierd"
 # llvm config
 export LLVM_PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
