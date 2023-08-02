53c7adebcbb31bea28803c46b536a16d79bdcd50
Vinci Xu <xsiyuan@gmail.com>
2023-08-02 14:21:45 +0800

    add noice

--- ikate/lua/plugins/ui.lua
+++ ikate/lua/plugins/ui.lua
@@ -1,11 +1,92 @@
 return {
-    { "theprimeagen/harpoon" },
-    { "theprimeagen/refactoring.nvim" },
-    { "folke/zen-mode.nvim" },
-    { "mbbill/undotree" },
-    { "tpope/vim-fugitive" },
-    { "echasnovski/mini.indentscope", version = false },
-    { "nvim-lualine/lualine.nvim" },
-    { "stevearc/dressing.nvim",       lazy = true },
-    { "SmiteshP/nvim-navic",          dependencies = "neovim/nvim-lspconfig" },
+	{ "theprimeagen/harpoon" },
+	{ "theprimeagen/refactoring.nvim" },
+	{ "folke/zen-mode.nvim" },
+	{ "mbbill/undotree" },
+	{ "tpope/vim-fugitive" },
+	{ "echasnovski/mini.indentscope", version = false },
+	{ "nvim-lualine/lualine.nvim" },
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		keys = {
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
+			},
+			{
+				"<leader>nl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
+			},
+			{
+				"<leader>nh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>na",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<leader>nd",
+				function()
+					require("noice").cmd("dismiss")
+				end,
+				desc = "Dismiss All",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll forward",
+				mode = {
+					"i",
+					"n",
+					"s",
+				},
+			},
+			{
+				"<c-b>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-b>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll backward",
+				mode = {
+					"i",
+					"n",
+					"s",
+				},
+			},
+		},
+	},
 }
