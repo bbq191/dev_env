a4c717ca42254769df9086110e885fd883fd467b
Vinci Xu <xsiyuan@gmail.com>
2023-08-07 17:41:49 +0800

    add: postgresql

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -12,8 +12,9 @@ export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
 export PRETTIERD_DEFAULT_CONFIG="$XDG_DATA_HOME/prettierd"
+export POSTGRES="/opt/homebrew/opt/postgresql@15/bin"
 # llvm config
-export LLVM_PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LLVM_PATH="/opt/homebrew/opt/llvm/bin"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
 
 # rust config
@@ -24,7 +25,7 @@ export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
 
 # path append
-export PATH="$CARGO_HOME/bin":"$LLVM_PATH":$PATH
+export PATH="$CARGO_HOME/bin:$LLVM_PATH:$POSTGRES:$PATH"
 # export PATH="$HOME/.local/bin":$PATH
 # eval "$(thefuck --alias)"
 eval "$(fnm env)"
