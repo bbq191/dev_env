26619ba610eba661ccb08d0b736a87af355a28d6
Vinci Xu <xsiyuan@gmail.com>
2023-08-11 13:35:15 +0800

    modify: lazyvim colorscheme

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "90"
 vim.opt.signcolumn = "yes"
