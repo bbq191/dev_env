cbcb24dc8312d552464ea91661991ed261e6e525
Vinci Xu <xsiyuan@gmail.com>
2023-08-16 16:14:24 +0800

    add: emacs base config

---
+++ ikatemacs/lisp/init-base.el
@@ -0,0 +1,389 @@
+;;; init-base.el --- The necessary settings -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(setq use-file-dialog nil ;; 关闭使用系统自带的文件选择对话框,使用Emacs自带的
+      use-dialog-box nil ;; 关闭使用系统自带的消息框,使用Emacs自带的
+      inhibit-x-resources t ;; 阻止加载X资源文件~/.Xresources中的设置
+      inhibit-default-init t ;; 阻止加载默认的init文件
+      inhibit-startup-screen t ;; 阻止显示启动画面
+      inhibit-startup-message t ;; 阻止显示启动消息
+      inhibit-startup-buffer-menu t) ;; 阻止显示启动缓冲区菜单
+
+;; Pixelwise resize
+(setq window-resize-pixelwise t ;; 设置窗口调整大小时以像素为单位
+      frame-resize-pixelwise t) ;; 设置框架调整大小时以像素为单位
+
+(setq-default bidi-paragraph-direction 'left-to-right) ;; 设置默认排版方向
+(setq bidi-inhibit-bpa t) ;; 禁用自动检测段落语言的重排算法
+
+;; No backup files
+(setq make-backup-files nil
+      auto-save-default nil)
+
+;; No lock files
+(setq create-lockfiles nil)
+
+;; Always load the newest file
+(setq load-prefer-newer t)
+
+;; Cutting and pasting use primary/clipboard
+(setq select-enable-primary t
+      select-enable-clipboard t)
+
+;; No gc for font caches
+(setq inhibit-compacting-font-caches t)
+
+;; Improve display
+(setq display-raw-bytes-as-hex t
+      redisplay-skip-fontification-on-input t)
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; 窗口启动最大化
+(delete-selection-mode 1) ;; 替换选中文字
+(setq display-line-numbers-type 'relative) ;; 设置为相对行号
+
+;; No annoying bell
+(setq ring-bell-function 'ignore)
+
+;; No eyes distraction
+(setq blink-cursor-mode nil)
+
+;; Smooth scroll & friends
+(setq scroll-step 2
+      scroll-margin 2
+      hscroll-step 2
+      hscroll-margin 2
+      scroll-conservatively 101
+      scroll-preserve-screen-position 'always)
+
+;; The nano style for truncated long lines.
+(setq auto-hscroll-mode 'current-line)
+
+;; Disable auto vertical scroll for tall lines
+(setq auto-window-vscroll nil)
+
+;; Dont move points out of eyes
+(setq mouse-yank-at-point t)
+
+(setq-default fill-column 80)
+
+;; Treats the `_' as a word constituent
+(add-hook 'after-change-major-mode-hook
+          (lambda ()
+            (modify-syntax-entry ?_ "w")))
+
+;; No tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Font size
+(set-face-attribute 'default nil :height 150)
+
+;; Sane defaults
+(setq use-short-answers t)
+
+;; Inhibit switching out from `y-or-n-p' and `read-char-choice'
+(setq y-or-n-p-use-read-key t
+      read-char-choice-use-read-key t)
+
+;; Enable the disabled narrow commands
+(put 'narrow-to-defun  'disabled nil)
+(put 'narrow-to-page   'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+
+;; Enable the disabled dired commands
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; Enable the disabled `list-timers', `list-threads' commands
+(put 'list-timers 'disabled nil)
+(put 'list-threads 'disabled nil)
+
+;; Quick editing in `describe-variable'
+(with-eval-after-load 'help-fns
+  (put 'help-fns-edit-variable 'disabled nil))
+
+;; Keep clean but enable `menu-bar' in MacOS
+(when (and (fboundp 'menu-bar-mode) (not (eq system-type 'darwin)))
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+;; Highlight parenthesises
+(use-package paren
+  :ensure nil
+  :hook (after-init . show-paren-mode)
+  :custom
+  (show-paren-when-point-inside-paren t)
+  (show-paren-when-point-in-periphery t))
+
+;; Show line/column number and more
+(use-package simple
+  :ensure nil
+  :custom
+  ;; show line/column/filesize in modeline
+  (line-number-mode t)
+  (column-number-mode t)
+  (size-indication-mode t)
+  ;; No visual feedback on copy/delete.
+  (copy-region-blink-delay 0)
+  (delete-pair-blink-delay 0)
+  ;; confusing if no fringes (GUI only).
+  (visual-line-fringe-indicators '(nil right-curly-arrow))
+  ;; don't save current clipboard text before replacing it
+  (save-interprogram-paste-before-kill nil)
+  ;; eliminate duplicates
+  (kill-do-not-save-duplicates t)
+  ;; include '\n' when point starts at the beginning-of-line
+  (kill-whole-line t)
+  ;; show cwd when `shell-command' and `async-shell-command'
+  (shell-command-prompt-show-cwd t)
+  ;; show the name of character in `what-cursor-position'
+  (what-cursor-show-names t)
+  ;; List only applicable commands.
+  ;;
+  ;; ``` elisp
+  ;; (defun foo ()
+  ;;   (interactive nil org-mode)
+  ;;   (message "foo"))
+  ;; ```
+  ;;
+  ;; M-x foo should only be available in `org-mode` or modes derived from `org-mode`.
+  (read-extended-command-predicate #'command-completion-default-include-p))
+
+;; Type text
+(use-package text-mode
+  :ensure nil
+  :custom
+  ;; better word wrapping for CJK characters
+  (word-wrap-by-category t)
+  ;; paragraphs
+  (sentence-end-double-space nil))
+
+;; Back to the previous position
+(use-package saveplace
+  :ensure nil
+  :hook (after-init . save-place-mode))
+
+;; Highlight current line in GUI
+(use-package hl-line
+  :ensure nil
+  :when (display-graphic-p)
+  :hook (after-init . global-hl-line-mode))
+
+;; Enable `repeat-mode' to reduce key sequence length
+;;
+;; If we have been idle for `repeat-exit-timeout' seconds, exit the repeated
+;; state.
+(use-package repeat
+  :ensure nil
+  :custom
+  (repeat-mode t)
+  (repeat-exit-timeout 3)
+  (repeat-exit-key (kbd "RET")))
+
+;; Server mode.
+;; Use emacsclient to connect
+(use-package server
+  :ensure nil
+  :hook (after-init . server-mode))
+
+;; Workaround with minified source files
+(use-package so-long
+  :ensure nil
+  :hook (after-init . global-so-long-mode))
+
+;; Completion engine
+(use-package minibuffer
+  :ensure nil
+  :bind (:map minibuffer-local-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-ns-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-completion-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-must-match-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-isearch-map
+         ([escape] . abort-recursive-edit))
+  :custom
+  ;; Default minibuffer is fine-tuned since Emacs 29
+  (completion-auto-help t)
+  (completion-show-help nil)
+  (completion-cycle-threshold nil)
+  (completion-auto-select 'second-tab)
+  (enable-recursive-minibuffers t)
+  (minibuffer-depth-indicate-mode t)
+  ;; shorten " (default %s)" => " [%s]"
+  (minibuffer-default-prompt-format " [%s]")
+  (minibuffer-electric-default-mode t)
+  ;; Don't insert completion at point into minibuffer
+  (minibuffer-completion-auto-choose nil)
+  ;; One frame one minibuffer.
+  (minibuffer-follows-selected-frame nil)
+  ;; Ignore cases when complete
+  (completion-ignore-case t)
+  (read-buffer-completion-ignore-case t)
+  (read-file-name-completion-ignore-case t)
+  ;; `selectrum', `vertico' and `icomplete' will honoring
+  (completion-styles '(basic partial-completion substring flex))
+  (completion-category-overrides '((buffer (styles . (flex)))))
+  ;; vertical view
+  (completions-format 'one-column)
+  (completions-max-height 13)
+  (completions-detailed t))
+
+
+;; window layout manager
+;;
+;; gt next-tab
+;; gT prev-tab
+(use-package tab-bar
+  :ensure nil
+  :hook (after-init . tab-bar-mode)
+  :custom
+  (tab-bar-show nil)
+  (tab-bar-tab-hints t)
+  (tab-bar-close-button-show nil)
+  (tab-bar-tab-name-function 'tab-bar-tab-name-all)
+  (tab-bar-format '(tab-bar-format-tabs tab-bar-separator)))
+
+;; transparent remote access
+(use-package tramp
+  :ensure nil
+  :defer t
+  :custom
+  ;; Always use file cache when using tramp
+  (remote-file-name-inhibit-cache nil)
+  (tramp-default-method "ssh"))
+
+;; Command line interpreter
+(use-package comint
+  :ensure nil
+  :bind (:map comint-mode-map
+         ([remap kill-region]   . backward-kill-word))
+  :custom
+  ;; No paging
+  (comint-pager "cat")
+  ;; Make the prompt of "*Python*" buffer readonly
+  (comint-prompt-read-only t)
+  (comint-history-isearch 'dwim)
+  ;; Colorize
+  (comint-terminfo-terminal "dumb-emacs-ansi"))
+
+;; Better abbrev expansion
+(use-package hippie-exp
+  :ensure nil
+  :bind ([remap dabbrev-expand] . hippie-expand)
+  :config
+  (defun try-expand-tempo (_old)
+    (require 'tempo)
+    (tempo-expand-if-complete))
+  :custom
+  (hippie-expand-try-functions-list '(try-expand-tempo
+                                      try-expand-dabbrev
+                                      try-expand-dabbrev-all-buffers
+                                      try-expand-dabbrev-from-kill
+                                      try-complete-file-name-partially
+                                      try-complete-file-name
+                                      try-expand-all-abbrevs
+                                      try-expand-list
+                                      try-expand-line
+                                      try-complete-lisp-symbol-partially
+                                      try-complete-lisp-symbol)))
+
+;; Buffer index
+(use-package imenu
+  :hook (imenu-after-jump . recenter))
+
+;; Buffer manager
+;;
+;; `sR': switch to saved filter groups
+(use-package ibuffer
+  :ensure nil
+  :hook (ibuffer-mode . ibuffer-auto-mode)
+  :bind ([remap list-buffers] . ibuffer)
+  :custom
+  (ibuffer-expert t)
+  (ibuffer-movement-cycle nil)
+  (ibuffer-show-empty-filter-groups nil)
+  (ibuffer-saved-filter-groups
+   '(("Default"
+      ("Emacs" (or (name . "\\*scratch\\*")
+                   (name . "\\*dashboard\\*")
+                   (name . "\\*compilation\\*")
+                   (name . "\\*Backtrace\\*")
+                   (name . "\\*Packages\\*")
+                   (name . "\\*Messages\\*")
+                   (name . "\\*Customize\\*")))
+      ("Browser" (or (mode . eww-mode)
+                     (mode . xwidget-webkit-mode)))
+      ("Help" (or (name . "\\*Help\\*")
+                  (name . "\\*Apropos\\*")
+                  (name . "\\*info\\*")
+                  (mode . Man-mode)
+                  (mode . woman-mode)))
+      ("Repl" (or (mode . gnuplot-comint-mode)
+                  (mode . inferior-emacs-lisp-mode)
+                  (mode . inferior-python-mode)))
+      ("Term" (or (mode . term-mode)
+                  (mode . shell-mode)
+                  (mode . eshell-mode)))
+      ("Mail" (or (mode . mail-mode)
+                  (mode . message-mode)
+                  (derived-mode . gnus-mode)))
+      ("Conf" (or (mode . yaml-mode)
+                  (mode . conf-mode)))
+      ("Dict" (or (mode . fanyi-mode)
+                  (mode . dictionary-mode)))
+      ("Text" (and (derived-mode . text-mode)
+                   (not (starred-name))))
+      ("Magit" (or (mode . magit-repolist-mode)
+                   (mode . magit-submodule-list-mode)
+                   (mode . git-rebase-mode)
+                   (derived-mode . magit-section-mode)
+                   (mode . vc-annotate-mode)))
+      ("VC" (or (mode . diff-mode)
+                (derived-mode . log-view-mode)))
+      ("Prog" (and (derived-mode . prog-mode)
+                   (not (starred-name))))
+      ("Dired" (mode . dired-mode))
+      ("IRC" (or (mode . rcirc-mode)
+                 (mode . erc-mode)))))))
+
+;; Recently opened files
+(use-package recentf
+  :ensure nil
+  :hook (after-init . recentf-mode)
+  :custom
+  (recentf-max-saved-items 300)
+  (recentf-auto-cleanup 'never)
+  (recentf-exclude '(;; Folders on MacOS start
+                     "^/private/tmp/"
+                     "^/var/folders/"
+                     ;; Folders on MacOS end
+                     "^/tmp/"
+                     "/ssh\\(x\\)?:"
+                     "/su\\(do\\)?:"
+                     "^/usr/include/"
+                     "/TAGS\\'"
+                     "COMMIT_EDITMSG\\'")))
+
+;; Try out emacs package without installing
+(use-package try
+  :ensure t
+  :commands try try-and-refresh)
+
+;; MacOS specific
+(use-package exec-path-from-shell
+  :ensure t
+  :when (eq system-type 'darwin)
+  :hook (after-init . exec-path-from-shell-initialize))
+
+(provide 'init-base)
+
+;;; init-base.el ends here
