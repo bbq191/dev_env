b1276ceb7080402342f5052626a51e42fd79a11c
Vinci Xu <xsiyuan@gmail.com>
2023-08-18 18:45:48 +0800

    add: basic config

--- emacs/config.el
+++ emacs/config.el
@@ -45,13 +45,12 @@
 ;; Block until current queue processed.
 (elpaca-wait)
 
-(defconst *is-a-mac* (eq system-type 'darwin))
+(defun reload-init-file ()
+  (interactive)
+  (load-file user-init-file)
+  (load-file user-init-file))
 
-(let ((no-border '(internal-border-width . 0)))
-  (add-to-list 'default-frame-alist no-border)
-  (add-to-list 'initial-frame-alist no-border))
-
-(defun sanityinc/adjust-opacity (frame incr)
+(defun ikate/adjust-opacity (frame incr)
   "Adjust the background opacity of FRAME by increment INCR."
   (unless (display-graphic-p frame)
     (error "Cannot adjust opacity of this frame"))
@@ -63,12 +62,47 @@
     (when (and (<= frame-alpha-lower-limit newalpha) (>= 100 newalpha))
       (modify-frame-parameters frame (list (cons 'alpha newalpha))))))
 
-(when (and *is-a-mac* (fboundp 'toggle-frame-fullscreen))
+(defconst macsys (eq system-type 'darwin))
+
+(let ((normal-gc-cons-threshold (* 512 1024 1024))
+      (init-gc-cons-threshold (* 1024 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+(with-no-warnings
+  ;; Key Modifiers
+  (setq mac-option-modifier 'meta
+        mac-command-modifier 'super)
+  (bind-keys ([(super a)] . mark-whole-buffer)
+              ([(super c)] . kill-ring-save)
+              ([(super l)] . goto-line)
+              ([(super q)] . save-buffers-kill-emacs)
+              ([(super s)] . save-buffer)
+              ([(super v)] . yank)
+              ([(super w)] . delete-frame)
+              ([(super z)] . undo)))
+
+(use-package diminish)
+
+(when (and macsys (fboundp 'toggle-frame-fullscreen))
   ;; Command-Option-f to toggle fullscreen mode
   ;; Hint: Customize `ns-use-native-fullscreen'
   (global-set-key (kbd "M-ƒ") 'toggle-frame-fullscreen))
 
 ;; TODO: use seethru package instead?
-(global-set-key (kbd "M-C-8") (lambda () (interactive) (sanityinc/adjust-opacity nil -2)))
-(global-set-key (kbd "M-C-9") (lambda () (interactive) (sanityinc/adjust-opacity nil 2)))
+(global-set-key (kbd "M-C-8") (lambda () (interactive) (ikate/adjust-opacity nil -2)))
+(global-set-key (kbd "M-C-9") (lambda () (interactive) (ikate/adjust-opacity nil 2)))
 (global-set-key (kbd "M-C-7") (lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
+
+(use-package toc-org
+    :commands toc-org-enable
+    :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(add-hook 'org-mode-hook 'org-indent-mode)
+(use-package org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(eval-after-load 'org-indent '(diminish 'org-indent-mode))
+
+(require 'org-tempo)
