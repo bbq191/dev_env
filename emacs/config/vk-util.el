6b6a1233985a35fff6d803eee5904675cc953f62
Vinci Xu <xsiyuan@gmail.com>
2023-09-13 17:23:58 +0800

    add: perspective

--- emacs/config/vk-util.el
+++ emacs/config/vk-util.el
@@ -21,6 +21,15 @@
           ("NOTE"       success bold)
           ("DEPRECATED" font-lock-doc-face bold))))
 
+(use-package perspective
+  :bind
+  ("C-x C-b" . persp-list-buffers)         ; or use a nicer switcher, see below
+  :custom
+  (persp-mode-prefix-key (kbd "C-c M-p"))  ; pick your own prefix key here
+  :init
+  (persp-mode))
+
+
 (provide 'vk-util)
 
 ;;; vk-util.el ends here
