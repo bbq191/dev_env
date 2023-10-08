7ae1963edd6c631c7d9ea5e4ddac48732ae8c5f9
Vinci Xu <xsiyuan@gmail.com>
2023-10-08 09:30:47 +0800

    add: greatest remap and opts

--- nvim/lua/user/core/keymaps.lua
+++ nvim/lua/user/core/keymaps.lua
@@ -1,32 +1,48 @@
 -- set leader key to space
 vim.g.mapleader = " "
+local k = vim.keymap -- for conciseness
 
-local keymap = vim.keymap -- for conciseness
-
----------------------
 -- General Keymaps -------------------
+k.set("n", "Q", "<nop>")
+-- k.set("n", "J", "mzJ`z")
+k.set("v", "J", ":m '>+1<CR>gv=gv")
+k.set("v", "K", ":m '<-2<CR>gv=gv")
+
+k.set("n", "<leader>pv", vim.cmd.Ex) -- file exploer
+
+k.set("n", "<C-d>", "<C-d>zz")
+k.set("n", "<C-u>", "<C-u>zz")
+k.set("n", "n", "nzzzv")
+k.set("n", "N", "Nzzzv")
 
 -- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+k.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+k.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+k.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+k.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+k.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+k.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+k.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+k.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+k.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+k.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+k.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+k.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+k.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- greatest remap ever
+k.set("n", "x", '"_x')
+k.set("x", "<leader>p", [["_dP]])
+k.set({"n", "v"}, "<leader>y", [["+y]])
+k.set("n", "<leader>Y", [["+Y]])
+k.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- next greatest remap ever : asbjornHaland
+k.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- nvim/lua/user/core/options.lua
+++ nvim/lua/user/core/options.lua
@@ -1,42 +1,47 @@
-local opt = vim.opt -- for conciseness
+local o = vim.opt -- for conciseness
 
 -- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+o.relativenumber = true -- show relative line numbers
+o.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+o.tabstop = 2 -- 2 spaces for tabs (prettier default)
+o.shiftwidth = 2 -- 2 spaces for indent width
+o.expandtab = true -- expand tab to spaces
+o.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+o.wrap = false -- disable line wrapping
 
 -- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+o.ignorecase = true -- ignore case when searching
+o.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+o.cursorline = true -- highlight the current cursor line
 
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+o.termguicolors = true
+o.background = "dark" -- colorschemes that can be light or dark will be made dark
+o.signcolumn = "yes" -- show sign column so that text doesn't shift
+o.scrolloff = 8 -- scroll more line
+o.colorcolumn = "80"
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+o.isfname:append("@-@")
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- o.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+o.splitright = true -- split vertical window to the right
+o.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
-opt.swapfile = false
+o.swapfile = false
+o.backup = false
+
