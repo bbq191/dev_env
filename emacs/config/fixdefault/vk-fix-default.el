f72ec03438695cda8657fa154018143e0bf89d85
Vinci Xu <xsiyuan@gmail.com>
2023-10-10 09:04:43 +0800

    fix: proxy

--- emacs/config/fixdefault/vk-fix-default.el
+++ emacs/config/fixdefault/vk-fix-default.el
@@ -18,6 +18,9 @@
   :hook (after-init . exec-path-from-shell-initialize)
   :config
   (setq exec-path-from-shell-debug t)
+  (exec-path-from-shell-copy-env "HTTPS_PROXY")
+  (exec-path-from-shell-copy-env "HTTP_PROXY")
+  (exec-path-from-shell-copy-env "ALL_PROXY")
   (exec-path-from-shell-copy-env "LLVM_HOME")
   (exec-path-from-shell-copy-env "LDFLAGS")
   (exec-path-from-shell-copy-env "CPPFLAGS")
