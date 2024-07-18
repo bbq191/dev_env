0c8930d0e8190d242f3773980f1125cc07f36f83
Vinci Xu <xsiyuan@gmail.com>
2024-07-18 15:00:26 +0800

    fix: update trouble

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -10,6 +10,17 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local transform_mod = require("telescope.actions.mt").transform_mod
+
+		local trouble = require("trouble")
+		local trouble_telescope = require("trouble.sources.telescope")
+
+		-- or create your custom action
+		local custom_actions = transform_mod({
+			open_trouble_qflist = function(prompt_bufnr)
+				trouble.toggle("quickfix")
+			end,
+		})
 
 		telescope.setup({
 			defaults = {
@@ -19,6 +30,7 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-t>"] = trouble_telescope.open,
 					},
 				},
 			},
